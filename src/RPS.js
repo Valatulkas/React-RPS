4e20c3a12739a1357e8504a6d8e3185cdb5c09da
jmferg11 <jfergie93@gmail.com>
2021-12-28 13:53:55 -0500

    finish logic and refactor into useEffect react hook

--- src/RPS.js
+++ src/RPS.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useDebugValue, useEffect, useState } from 'react';
 import './index.css';
 
 const RPS = () => {
@@ -11,7 +11,6 @@ const RPS = () => {
   const handleClick = (e) => {
     setUserChoice(e);
     generateComputerChoice();
-    check();
   }
 
   const generateComputerChoice = () => {
@@ -20,31 +19,35 @@ const RPS = () => {
     setComputerChoice(randomChoice);
   }
 
-  const check = () => {
-    switch(userChoice + computerChoice) {
-      case 'scissorspaper':
-      case 'rockscissors':
-      case 'paperrock':
-      setResult('Computer is victorious!')
-        break
-      case 'paperscissors':
-      case 'scissorsrock':
-      case 'rockpaper':
-      setResult('You`re victorious!')
-        break
-      case 'rockrock':
-      case 'paperpaper':
-      case 'scissorsscissors':
-      setResult('Draw! Play again')
-        break
+  useEffect(() => {
+    {
+      switch  (userChoice + computerChoice) {
+        case 'scissorspaper':
+        case 'rockscissors':
+        case 'paperrock':
+          setResult('You`re victorious!')
+          break
+        case 'paperscissors':
+        case 'scissorsrock':
+        case 'rockpaper':
+          setResult('Computer is victorious!')
+          break
+        case 'rockrock':
+        case 'paperpaper':
+        case 'scissorsscissors':
+          setResult('Draw! Play again')
+          break
+      }
     }
-  }
+  }, [computerChoice, userChoice]); 
 
   return (
     <div>
         <h1>User choice is: {userChoice}</h1>
         <h1>Computer choice is: {computerChoice}</h1>
-        {choices.map((choice, index) => <button key={index} onClick={() => handleClick(choice)}>{choice}</button>)}
+        {choices.map((choice, index) => 
+          <button key={index} onClick={() => handleClick(choice)}>{choice}</button>
+        )}
         <h2>{result}</h2>
     </div>
   );
