f79d8c3823334339be1ec7377b33bff9b5fd6d44
jmferg11 <jfergie93@gmail.com>
2021-12-28 12:56:45 -0500

    initial setup of functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [userChoice, setUserChoice] = useState(null);
+  
+  const handleClick = (e) => {
+    setUserChoice(e)
+  }
+
   return (
-    <div className="App">
-      <h2>HALLO</h2>
+    <div>
+        <h1>User choice is: {userChoice}</h1>
+        <h1>Computer choice is:</h1>
+        <button onClick={() => handleClick('rock')}>Rock</button>
+        <button onClick={() => handleClick('paper')}>Paper</button>
+        <button onClick={() => handleClick('scissors')}>Scissors</button>
     </div>
   );
 }
